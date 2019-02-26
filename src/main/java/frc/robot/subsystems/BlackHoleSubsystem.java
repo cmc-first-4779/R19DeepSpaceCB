e08254489d4a841b40c201704d510f84772895ae
khope17 <kevin.hope@gmail.com>
2019-02-26 12:35:13 -0500

    Fixed the issued for the checking the box spinning level.

--- src/main/java/frc/robot/subsystems/BlackHoleSubsystem.java
+++ src/main/java/frc/robot/subsystems/BlackHoleSubsystem.java
@@ -17,7 +17,6 @@ import frc.robot.Robot;
 import frc.robot.RobotMap;
 import frc.robot.commands.BlackHole.BlackHoleRotateWithJoystickCommand;
 
-
 /**
  * Add your docs here.
  */
@@ -25,27 +24,26 @@ public class BlackHoleSubsystem extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
-  //Declare our Spinmotor controller as a Talon
+  // Declare our Spinmotor controller as a Talon
   WPI_TalonSRX spinMotor;
-  //Declare our Plunger as a Double Solenoid
+  // Declare our Plunger as a Double Solenoid
   DoubleSolenoid plunger;
 
-
-  //Angle the box should be at
+  // Angle the box should be at
   double boxAngle = 0;
 
   public BlackHoleSubsystem() {
-    //Initiate our plunger and our spinMotor and our limit switch and our counter for the limit switch
+    // Initiate our plunger and our spinMotor and our limit switch and our counter
+    // for the limit switch
     plunger = new DoubleSolenoid(RobotMap.PCM_PORT_PLUNGER_PLUNGE, RobotMap.PCM_PORT_PLUNGER_RETRACT);
     spinMotor = new WPI_TalonSRX(RobotMap.CAN_ADDRESS_BLACKHOLE);
 
-
-    //Initialize the Talon settings on the spinMotor
+    // Initialize the Talon settings on the spinMotor
     Robot.initMotorController(spinMotor);
-    //Config the encoder on the spinMotor
+    // Config the encoder on the spinMotor
     spinMotor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
     spinMotor.setSelectedSensorPosition(0, 0, 0);
-    //Set our PID values for our spinMotor
+    // Set our PID values for our spinMotor
     spinMotor.config_kP(0, 1, 0);
     spinMotor.config_kI(0, 0, 0);
     spinMotor.config_kD(0, 0, 0);
@@ -66,50 +64,49 @@ public class BlackHoleSubsystem extends Subsystem {
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
-    //Default Command:   BLACK HOLE STAYS WHERE IT IS AT!!
+    // Default Command: BLACK HOLE STAYS WHERE IT IS AT!!
     setDefaultCommand(new BlackHoleRotateWithJoystickCommand());
   }
 
-  //Spin the Black Hole Cargo Box around
-/*   public void spin(double rate) {
-    // spin the spinner forwards at a set speed
-    if (rate > 0.25){
-      spinMotor.set(ControlMode.PercentOutput, RobotMap.BLACK_HOLE_SPIN_FORWARD_SPEED);
-    } else if (rate < -0.25) {
-      spinMotor.set(ControlMode.PercentOutput, RobotMap.BLACK_HOLE_SPIN_REVERSE_SPEED);
-    } else {
-      spinMotor.set(ControlMode.PercentOutput, 0);
-    }
-  } */
+  // Spin the Black Hole Cargo Box around
+  /*
+   * public void spin(double rate) { // spin the spinner forwards at a set speed
+   * if (rate > 0.25){ spinMotor.set(ControlMode.PercentOutput,
+   * RobotMap.BLACK_HOLE_SPIN_FORWARD_SPEED); } else if (rate < -0.25) {
+   * spinMotor.set(ControlMode.PercentOutput,
+   * RobotMap.BLACK_HOLE_SPIN_REVERSE_SPEED); } else {
+   * spinMotor.set(ControlMode.PercentOutput, 0); } }
+   */
 
-  //Spin the Black Hole to a certain setpoint.
+  // Spin the Black Hole to a certain setpoint.
   public void rotateToSetPoint(double angle) {
     // do the math to figure out what the encoder count should be
     // Move arm to set point
     boxAngle = angle;
     spinMotor.set(ControlMode.Position, angle);
-    //System.out.println("Encoder count: " + spinMotor.getSelectedSensorPosition());
+    // System.out.println("Encoder count: " +
+    // spinMotor.getSelectedSensorPosition());
   }
 
-  //Spin the Black Hole to a certain setpoint.
+  // Spin the Black Hole to a certain setpoint.
   public void rotateToSetPoint() {
     // do the math to figure out what the encoder count should be
     // Move arm to set point if valid
-    if (validAngleSet()){
-    spinMotor.set(ControlMode.MotionMagic, boxAngle);
+    if (validAngleSet()) {
+      spinMotor.set(ControlMode.MotionMagic, boxAngle);
     } else {
       SmartDashboard.putString("BlackHoleSubsystem", "Angle: " + boxAngle + " Out of valid Range");
     }
-    //spinMotor.set(ControlMode.Position, boxAngle);
-    //System.out.println("Encoder count: " + spinMotor.getSelectedSensorPosition());
+    // spinMotor.set(ControlMode.Position, boxAngle);
+    // System.out.println("Encoder count: " +
+    // spinMotor.getSelectedSensorPosition());
   }
 
-
-  //   Return TRUE if we are rotating the Blackhole Box with valid angles..
-  //    Return FALSE if we are rotating it out of range...
-  //    Don't want to break the box...   
+  // Return TRUE if we are rotating the Blackhole Box with valid angles..
+  // Return FALSE if we are rotating it out of range...
+  // Don't want to break the box...
   private boolean validAngleSet() {
-    if (boxAngle > RobotMap.BLACK_HOLE_MAX_NEGATIVE_ANGLE && boxAngle < RobotMap.BLACK_HOLE_MAX_POSITIVE_ANGLE) {
+    if (boxAngle > RobotMap.BLACK_HOLE_MAX_NEGATIVE_ANGLE * 4096/360 && boxAngle < RobotMap.BLACK_HOLE_MAX_POSITIVE_ANGLE * 4096/360) {
       return true;
     } else {
       return false;
@@ -121,34 +118,34 @@ public class BlackHoleSubsystem extends Subsystem {
     plunger.set(DoubleSolenoid.Value.kReverse);
   }
 
-  //Retract the plunger after ejecting the ball...
+  // Retract the plunger after ejecting the ball...
   public void retractPlunger() {
     plunger.set(DoubleSolenoid.Value.kForward);
   }
 
-  //Stop the motor from spinning!!
+  // Stop the motor from spinning!!
   public void stop() {
     spinMotor.stopMotor();
   }
 
-  //Reset the encoder on our Arm Master Talon.
+  // Reset the encoder on our Arm Master Talon.
   public void resetEncoder() {
     spinMotor.setSelectedSensorPosition(0);
   }
 
-  //  Return the Encoder Position for the box..
+  // Return the Encoder Position for the box..
   public double getEncoderPosition() {
     return spinMotor.getSelectedSensorPosition();
   }
 
-  //  Set our Box Angle 
+  // Set our Box Angle
   public void setBoxAngle(double angle) {
     System.out.println("Setting BoxAngle to " + angle);
     boxAngle = angle;
   }
 
-//   Return our Box angle
-  public double getBoxAngle(){
+  // Return our Box angle
+  public double getBoxAngle() {
     return boxAngle;
   }
 
