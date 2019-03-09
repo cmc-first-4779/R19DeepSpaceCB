9180ccf0a66ca02c4098bff547dbd7a743657f5e
mbhoxsie <matt.hoxsie@gmail.com>
2019-03-09 11:48:48 -0500

    Adjusted OI for deploy commands.

--- src/main/java/frc/robot/commands/Arms/ArmsResetEncoderCommand.java
+++ src/main/java/frc/robot/commands/Arms/ArmsResetEncoderCommand.java
@@ -32,7 +32,7 @@ public class ArmsResetEncoderCommand extends Command {
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return ;
+    return true;
   }
 
   // Called once after isFinished returns true
